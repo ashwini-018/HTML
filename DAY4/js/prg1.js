6f99bb550fe3ab9e6d1b20a669e7e502fc5ea69b
ashwini-018 <aswiniaishu213@gmail.com>
2025-10-14 09:32:26 +0530

    Add files via upload

---
+++ DAY4/js/prg1.js
@@ -0,0 +1,22 @@
+
+     console.log("Hello world");
+     
+     var msg="I am  hari";
+     console.log(msg);
+
+     let mssg="I am  hari";
+      console.log(msg);
+      const greeting ={
+        text : "welcome",
+        sender: "system"
+      };
+      console.log(greeting);
+
+      function hellofunction() {
+        console.log("hello from function");
+      }
+      hellofunction();
+      helloArrowfunction = () => {
+        console.log("hello from Arrow function ");
+      }
+      helloArrowfunction();
